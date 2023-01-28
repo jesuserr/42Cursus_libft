2fa2beff49a5b9a35fc0b55249e29af020deda23
Jesus Serrano <118769872+ivoice666@users.noreply.github.com>
2023-01-28 15:55:00 +0100

    Add files via upload

---
+++ Libft/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jesuserr <jesuserr@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 14:38:15 by jesuserr          #+#    #+#             */
+/*   Updated: 2023/01/26 17:51:58 by jesuserr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{	
+	t_list	*temp;
+
+	if (lst == NULL)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		(*lst) = temp;
+	}
+}
